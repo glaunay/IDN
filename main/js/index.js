9fe05f8ed10341e68b32f3cda9925a631db39e41
deniaudm <madeline.deniaud@imag.fr>
2015-06-18 15:59:41 +0200

    Amelioration 5 nvlle mep

--- main/js/index.js
+++ main/js/index.js
@@ -15,86 +15,47 @@ function mainPageSet (){
     $('#toggleOne').on('click',function(event){	   
 			   $('.mainContent:not(#contentOne)').hide();
 			   $('.mainContent#contentOne').show();
-			 //  $('#toggleThree').addClass('blockOneAccordeonBottom');
-			 // $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-			  // $('#toggleNine').addClass('accordion-base');
 		       });
     
     $('#toggleTwo').on('click',function(){
 			   $('.mainContent:not(#contentTwo)').hide();
 			   $('.mainContent#contentTwo').show();
-			   
-			   //$('#toggleThree').addClass('blockOneAccordeonBottom');
-			 //  $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-			//   $('#toggleNine').addClass('accordion-base');
 		       });
     
     $('#toggleThree').on('click',function(){
 			     $('.mainContent:not(#contentThree)').hide();
 			     $('.mainContent#contentThree').show();
-			     //$('#toggleThree').removeClass('blockOneAccordeonBottom');
-
-			//     $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-			//     if (isSelected(this))
-			//	 $('#toggleThree').addClass('blockOneAccordeonBottom');
-			 //    $('#toggleNine').addClass('accordion-base');
 			 });
     
     $('#toggleFour').on('click',function(){
 			    $('.mainContent:not(#contentFour)').hide();
 			    $('.mainContent#contentFour').show();
-
-			    //$('#toggleThree').addClass('blockOneAccordeonBottom');
-			//    $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-			 //   $('#toggleNine').addClass('accordion-base');
 			});
     
     $('#toggleFive').on('click',function(){
 			    $('.mainContent:not(#contentFive)').hide();
 			    $('.mainContent#contentFive').show();
-			 //   $('#toggleNine').addClass('accordion-base');
 			});
     
     $('#toggleSix').on('click',function(){
 			   $('.mainContent:not(#contentSix)').hide();
 			   $('.mainContent#contentSix').show();
-
-			   //$('#toggleThree').addClass('blockOneAccordeonBottom');
-			 //  $('#toggleSeven').addClass('blockTwoAccordeonBottom'); 
-			 //  $('#toggleNine').addClass('accordion-base');
 		       });
     
     $('#toggleSeven').on('click',function(){
 			     $('.mainContent:not(#contentSeven)').hide();
 			     $('.mainContent#contentSeven').show();
-			   
-			  //   if(!isSelected(this))
-			//	 $('#toggleSeven').removeClass('blockTwoAccordeonBottom');
-			  //   else 
-			//	 $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-
-			     //$('#toggleThree').addClass('blockOneAccordeonBottom');
-			   //  $('#toggleNine').addClass('accordion-base');
 			 });
     
     $('#toggleEight').on('click',function(){
 			     $('.mainContent:not(#contentEight)').hide();
 			     $('.mainContent#contentEight').show();
-
 			     
-			    // $('#toggleThree').addClass('blockOneAccordeonBottom');
-			   //  $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-			   //  $('#toggleNine').addClass('accordion-base');			     
 			 });
  
    $('#toggleNine').on('click',function(){
 			     $('.mainContent:not(#contentNine)').hide();
 			     $('.mainContent#contentNine').show();
-
-			     
-			     //$('#toggleThree').addClass('blockOneAccordeonBottom');
-			   //  $('#toggleSeven').addClass('blockTwoAccordeonBottom');
-			   //  $(this).toggleClass('accordion-base');
 
 			 });
 
